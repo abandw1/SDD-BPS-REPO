f4d17828ba87d8ac5f33422383d123d1f07aad2b
Finnegan O <theonlyuser@Finnegans-Air.modem>
2020-05-20 14:28:18 +1000

    Commiting to fix reverts. (This GitHub thing is tough).

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:Script.js
+
 // JavaScript source code
 
 var elements = [['','','','','',''],['','','','','',''],['','','','','',''],['','','','','',''],['','','','','',''],['','','','','','']];
@@ -325,74 +325,4 @@ $(".block").click(function() {
 	}
 
 }})
-=======
-// JavaScript source code
 
-var elements = [['','','','','',''],['','','','','',''],['','','','','',''],['','','','','',''],['','','','','',''],['','','','','','']];
-
-function clearElements(){
-
-	for (var i = 0; i < 6; i++) {
-		for (var j = 0; j < 6; j++) {
-			document.getElementsByClassName("block_" + j)[i].innerHTML = '';
-			
-		}	
-	}
-}
-
-function submitElements(){
-	var count = 0;
-	for (var i = 0; i < 6; i++) {
-		for (var j = 0; j < 6; j++) {
-
-			elements[i][j] = document.getElementsByClassName("block_" + j)[i].innerHTML;
-
-		}
-	}
-
-}
-
-
-function solveGrid(){
-	
-	
-	
-	
-
-
-
-
-	
-}
-
-
-
-
-
-
-
-
-
-
-$(".block").click(function() {
-	var bttnClass = this.className;
-	var col = bttnClass.charAt(6);
-	var row = 	$(this).parent().attr("id").charAt(3);
-	switch(this.innerHTML){	
-		case '':
-		this.innerHTML = '0';
-		break;
-		case '0':
-		this.innerHTML = '1';
-		break;
-		case '1':
-		this.innerHTML = '';
-		break;
-		default:
-		alert('ooopss');
-
-	}
-
-});
-	
->>>>>>> BPS-REPO/master:script.js
